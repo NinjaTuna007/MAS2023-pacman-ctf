c463d6538f55595466a895321a4f667d9eb907fb
NinjaTuna007 <94217791+NinjaTuna007@users.noreply.github.com>
2023-05-07 12:49:00 +0200

    minor mods

--- myTeam.py
+++ myTeam.py
@@ -16,6 +16,7 @@ from captureAgents import CaptureAgent
 import random, time, util
 from game import Directions
 import game
+import math
 
 #################
 # Team creation #
@@ -77,7 +78,130 @@ class DummyAgent(CaptureAgent):
     Your initialization code goes here, if you need any.
     '''
 
+  # evaluation function
+  def evaluationFunction(self, gameState):
+    # heuristic function to evaluate the state of the game
 
+    # if terminal state    
+    if gameState.isOver():
+      return gameState.getScore()
+    
+    if False:
+      # if pacman is eaten: seen by spawning at the start
+      if gameState.getAgentState(self.index).isPacman and gameState.getAgentState(self.index).configuration.pos == gameState.getInitialAgentPosition(self.index):
+        return -math.inf
+      
+      # if pacman eats a food
+      if gameState.getAgentState(self.index).numCarrying > 0:
+        return math.inf
+      
+      # if pacman is close to a food
+      foodList = self.getFood(gameState).asList()
+      minFoodDistance = min([self.getMazeDistance(gameState.getAgentPosition(self.index), food) for food in foodList])
+      if minFoodDistance < 3:
+        return 1/minFoodDistance
+      
+      # if pacman is close to a ghost
+      ghostList = [gameState.getAgentPosition(i) for i in self.getOpponents(gameState) if not gameState.getAgentState(i).isPacman]
+      # filter out ghosts that are not visible
+      ghostList = [ghost for ghost in ghostList if ghost!=None]
+
+      if any(ghostList):
+        minGhostDistance = min([self.getMazeDistance(gameState.getAgentPosition(self.index), ghost) for ghost in ghostList])
+        if minGhostDistance < 3:
+          return -1/minGhostDistance
+      
+      # if pacman is close to a capsule
+      capsuleList = self.getCapsules(gameState)
+      minCapsuleDistance = min([self.getMazeDistance(gameState.getAgentPosition(self.index), capsule) for capsule in capsuleList])
+      if minCapsuleDistance < 3:
+        return 1/minCapsuleDistance
+      
+      return 0
+      
+    else:
+      # new heuristic function
+      # list of our food, enemy food, enemy pacman, enemy ghost, our pacman, our ghost, capsule
+      foodList = self.getFood(gameState).asList()
+      enemyFoodList = self.getFoodYouAreDefending(gameState).asList()
+      enemyPacmanList = [gameState.getAgentPosition(i) for i in self.getOpponents(gameState) if gameState.getAgentState(i).isPacman]
+      enemyGhostList = [gameState.getAgentPosition(i) for i in self.getOpponents(gameState) if not gameState.getAgentState(i).isPacman]
+      enemyGhostList = [ghost for ghost in enemyGhostList if ghost!=None]
+      ourPacmanList = [gameState.getAgentPosition(i) for i in self.getTeam(gameState) if gameState.getAgentState(i).isPacman]
+      ourGhostList = [gameState.getAgentPosition(i) for i in self.getTeam(gameState) if not gameState.getAgentState(i).isPacman]
+      ourGhostList = [ghost for ghost in ourGhostList if ghost!=None]
+      capsuleList = self.getCapsules(gameState)
+
+      # if terminal state
+      if gameState.isOver():
+        return gameState.getScore()
+      
+      
+      
+
+
+      return 0
+
+    
+
+  # setup for minimax
+  def getSuccessors(self, gameState):
+    """
+    Returns successor states, the actions they require, and a cost of 1.
+
+    The following successor states are the same board, but the agent
+    has been moved one step in the specified direction.
+
+    """
+    successors = []
+    for action in gameState.getLegalActions(self.index):
+      # Ignore illegal actions
+      if action == Directions.STOP:
+        continue
+      successor = gameState.generateSuccessor(self.index, action)
+      successors.append((successor, action))
+    return successors
+  
+  def max_agent(self, gameState, depth, time_left = math.inf, alpha = -math.inf, beta = math.inf):
+    if depth == 0 or time_left < 0.05:
+      return self.evaluationFunction(gameState)
+    v = -math.inf
+    for successor, action in self.getSuccessors(gameState):
+
+      # check if enemy is visible from the successor state
+      enemyList = [successor.getAgentPosition(i) for i in self.getOpponents(successor)]
+      if any(enemyList):
+        v = max(v, self.min_agent(successor, depth-1, time_left - 0.05, alpha, beta))
+      
+      else:
+        # if enemy is not visible, then assume board remains the same and make next move
+        v = max(v, self.max_agent(successor, depth - 1, time_left - 0.05, alpha, beta))
+
+      if v >= beta:
+        return v
+      alpha = max(alpha, v)
+    return v
+  
+  def min_agent(self, gameState, depth, time_left = math.inf, alpha = -math.inf, beta = math.inf):
+    if depth == 0 or time_left < 0.05:
+      return self.evaluationFunction(gameState)
+    v = math.inf
+    for successor, action in self.getSuccessors(gameState):
+        
+        # check if enemy is visible from the successor state
+        enemyList = [successor.getAgentPosition(i) for i in self.getOpponents(successor)]
+        if any(enemyList):
+          v = min(v, self.max_agent(successor, depth-1, time_left - 0.05, alpha, beta))
+        
+        else:
+          # if enemy is not visible, then assume board remains the same and make next move
+          v = min(v, self.min_agent(successor, depth - 1, time_left - 0.05, alpha, beta))
+  
+        if v <= alpha:
+          return v
+        beta = min(beta, v)
+    return v
+  
   def chooseAction(self, gameState):
     """
     Picks among actions randomly.
@@ -88,5 +212,17 @@ class DummyAgent(CaptureAgent):
     You should change this in your own agent.
     '''
 
+    # minimax
+    depth = 5
+    time_left = 0.95 # time left for the agent to choose an action
+    best_action = None
+    best_score = -math.inf
+    for successor, action in self.getSuccessors(gameState):
+      score = self.min_agent(successor, depth, time_left - 0.05)
+      if score > best_score:
+        best_score = score
+        best_action = action
+    return best_action
+  
     return random.choice(actions)
 
